edad222fda635c27fd1ea4dcb571c376ed5bf4ca
Mohanrajcgm <103239123+Mohanrajcgm@users.noreply.github.com>
2022-05-20 16:07:18 +0530

    Add files via upload

---
+++ honest.js
@@ -0,0 +1,12 @@
+let count=10;
+window.setTimeout(function timer(){
+    document.querySelector(".timer").innerText="honest"
+         window.setTimeout(()=>{
+            document.querySelector(".timer").innerText="Don't get tension"
+           window.setTimeout(()=>{
+            document.querySelector(".timer").innerText="Be cool super cool😎😎"
+              }, 1*1000)
+             
+            }, 1*1000)
+            
+     }, 1*1000)
